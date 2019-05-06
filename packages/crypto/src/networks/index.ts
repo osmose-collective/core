4cf3000cab48f3d415e801c748e24f281693d7c0
Zaraki <fpalazo@gmail.com>
2019-05-06 18:27:55 +0200

    Fetch changes from ark repository, osmose testnet network config is still broken for docker

--- packages/crypto/src/networks/index.ts
+++ packages/crypto/src/networks/index.ts
@@ -1,7 +1,7 @@
 import { devnet } from "./devnet";
 import { mainnet } from "./mainnet";
-import { testnet } from "./testnet";
 import { osmose_testnet } from "./osmose_testnet";
+import { testnet } from "./testnet";
 import { unitnet } from "./unitnet";
 
 export type INetwork = typeof mainnet.network | typeof devnet.network | typeof testnet.network | typeof unitnet.network | typeof osmose_testnet.network;
